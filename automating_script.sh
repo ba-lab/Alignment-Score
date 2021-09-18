3e023f405d0676acd821fc279f7e7b5c7ac638f2
Bikash Shrestha <bikash_stha637@hotmail.com>
2021-09-18 10:27:32 -0500

    Update automating_script.sh

--- automating_script.sh
+++ automating_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Author: Bikash Shrestha, University of Missouri-St. Louis, 07-23-2021
+# Description: This script runs the pipeline. It requires an MSA file and ID of the target
+
 if [ $# -eq 0 ]
 then
     echo "Input MSA was not provided"
